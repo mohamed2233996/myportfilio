9c332765415d0465949ca6532bbe174874d4f192
mohamed2233996 <medomano771@gmail.com>
2026-04-22 21:39:33 +0200

    v1.1

--- app/lib/supabase.ts
+++ app/lib/supabase.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL as string;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY as string;
 
 if (!supabaseUrl || !supabaseAnonKey) {
     console.error("❌ Supabase environment variables are missing!");
